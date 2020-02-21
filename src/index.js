813fb937db5936d0b034a638b788378cb3a7ad90
Juan Castro Fernández <castrofernandez@gmail.com>
2020-02-21 19:12:48 +0100

    Adds parameter validation.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 /* eslint-disable require-jsdoc */
 'use strict';
 
+import setupme from 'setupme';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+const LOG_NAME = 'containme';
+
+const DEFAULT_OPTIONS = {
+    component: () => {},
+    actions: {},
+    mapStateToProps: () => {},
+};
+
+const areParamsValid = (params) => setupme
+    .validate(DEFAULT_OPTIONS, params, {logName: LOG_NAME, strictly: false, deeply: false}).success;
+
 const mapDispatchToProps = (actions) => (dispatch) => bindActionCreators(actions, dispatch);
 
-export default ({component, actions = {}, mapStateToProps = (state) => state}) => {
-    return connect(mapStateToProps, mapDispatchToProps(actions))(component);
+export default (params = {}) => {
+    const {component, actions = {}, mapStateToProps = (state) => state} = params;
+
+    return areParamsValid(params) ?
+        connect(mapStateToProps, mapDispatchToProps(actions))(component) :
+        () => {};
 };
